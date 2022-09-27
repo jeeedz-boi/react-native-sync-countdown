ac129425dcbe5507b548e0f7949a62beca51ac17
Thongchai Yamsuk <thongchai@bluebik.com>
2022-09-27 16:36:20 +0700

    test: update example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,18 +1,29 @@
-import * as React from 'react';
-
-import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'react-native-sync-countdown';
+import React, { useEffect } from 'react';
+import { StatusBar, StyleSheet, Text, View } from 'react-native';
+import { CountDown } from 'react-native-sync-countdown';
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
+  const countDown = CountDown;
 
-  React.useEffect(() => {
-    multiply(3, 7).then(setResult);
+  useEffect(() => {
+    countDown.setTimer(6000);
+
+    countDown.getTimer((timer) => {
+      console.log('get timer every 1 sec', timer);
+    });
+
+    countDown.getTimerOnEnd(() => {
+      console.log('timer end');
+    });
+
+    return () => countDown.resetTimer();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar />
     </View>
   );
 }
@@ -20,12 +31,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  box: {
-    width: 60,
-    height: 60,
-    marginVertical: 20,
   },
 });
